4c1f5d8bbc08e57d52916b6a0cdeeb82760b9aa3
Jawad Abbasi <jawadabbasi1107@gmail.com>
2025-07-26 20:52:43 +0500

    Build signup logic with template

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,1 +1,8 @@
-from dj
+# accounts/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # yeh view 127.0.0.1:8000/ handle kare ga
+    path('signup/', views.signup, name='signup'),
+]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Well come to MJ_GLOBAL_STORE home page !")
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Home Page")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+
+        print("User input:", username, password, email)
+        return HttpResponse("Signup successfully jawad!")
+
+    return render(request, "accounts/signup.html")
+
