1a3f02c2e069f4a96c2059868abac2647d7909f4
Jawad Abbasi <jawadabbasi1107@gmail.com>
2025-07-28 19:54:19 +0500

    updated accounts/views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,25 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Well come to MJ_GLOBAL_STORE home page !")
-
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Home Page")
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def signup(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
         email = request.POST['email']
-        feedback = request.POST['feedback']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
 
-        print("User input:", username, password, email)
-        return HttpResponse("Signup successfully jawad!")
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username already taken!")   
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, "Email already registered!")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                messages.success(request, "Signup successful!")
+                return redirect('home')  # ya login page
+        else:
+            messages.error(request, "Passwords do not match!")
+            return redirect('signup')
 
-    return render(request, "accounts/signup.html")
-
+    return render(request, 'signup.html')
