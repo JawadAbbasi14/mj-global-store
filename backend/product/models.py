f62dea2ec13391c0a5d01a8f6eefa41a0413625f
Jawad Abbasi <jawadabbasi1107@gmail.com>
2025-08-15 23:21:03 +0500

    model.py

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,11 +1,15 @@
-# Create your models here.
 from django.db import models
-
-class Product(models.Model):
+class Products(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.IntegerField()
+    stock = models.IntegerField(default=0)
+    description = models.TextField(max_length=1000)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = "product_table"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = ['name']
