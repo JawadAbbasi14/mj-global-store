b0cffbc914658573605e43051c71aba0e1c6b0a1
Jawad Abbasi <jawadabbasi1107@gmail.com>
2025-08-24 17:53:38 +0500

    enhance store styling

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
 
-      path("",views.home,name="home"), # ← bas empty string lagao
+      path("",views.home,name="home"),
+       path("about/",views.about,name="about"),
+       path("contact/",views.contact,name="contact"),
+       path("polices/",views.privecy_and_policy,name="policies"),
+       path("contitions/",views.terms_and_contitions,name="terms"),
+
 ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"home/home.html")
+
+def about(request):
+    return render(request,"home/about.html")
+
+def contact(request):
+    return render(request,"home/contact.html")
+
+def privecy_and_policy(request):
+    return render(request,"home/privecy_and_policy.html")
+
+def terms_and_contitions(request):
+    return render(request,"home/terms_and_contitions.html")
