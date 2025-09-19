ff68e899738b30ddb3129866b24a9ebed3cf6bfd
Jawad Abbasi <jawadabbasi1107@gmail.com>
2025-09-19 12:18:56 +0500

    complete the first phase of my project Basic Lavel

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -3,7 +3,7 @@ from .models import Products
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'stock')   # list view
+    list_display = ('id', 'name', 'price', 'stock','image')   # list view
     search_fields = ('name',)                        # search option
     list_filter = ('price',)                         # filter option
 
--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls.static import static
-from django.conf import settings
+
 from django.urls import path
 from . import views
 
@@ -9,7 +8,4 @@ urlpatterns = [
 ]
 
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
